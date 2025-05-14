50cdd6497fb1e76aff5d9e2cf77b0dacbe15a255
Matthew Budde <mdbudde@users.noreply.github.com>
2025-05-14 18:05:15 -0500

    fix for compile

--- relProtos_p.h
+++ relProtos_p.h
@@ -1,8 +1,8 @@
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/initMeth.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/initMeth.c */
 void initMeth(void);
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/loadMeth.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/loadMeth.c */
 void loadMeth(const char *);
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/parsRelations.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/parsRelations.c */
 void ExcPulse1EnumRelation(void);
 void ExcPulse1AmplRel(void);
 void HandleRFPulseAmplitude(void);
@@ -11,23 +11,23 @@ void ExcPulse1Range(void);
 void EffSWhRange(void);
 void EffSWhRel(void);
 void MyRgInitRel(void);
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/BaseLevelRelations.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/BaseLevelRelations.c */
 void SetBaseLevelParam(void);
 void SetBasicParameters(void);
 void SetFrequencyParameters(void);
 void SetGradientParameters(void);
 void SetMachineParameters(void);
 void SetPpgParameters(void);
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/RecoRelations.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/RecoRelations.c */
 void SetRecoParam(void);
 void RecoDerive(void);
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/backbone.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/backbone.c */
 void backbone(void);
 void UpdateTotalTime(void);
 void UpdateRFPulses(void);
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/deriveVisu.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/deriveVisu.c */
 void deriveVisu(void);
-/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/pulseqV1/pulseqRelations.c */
+/* /opt/PV6.0.1/prog/curdir/budde/ParaVision/methods/src/bruker_pulseq/pulseqRelations.c */
 void UpdatePulseq(void);
 void UpdateSeqList(void);
 void PulseqFileHandler(void);
